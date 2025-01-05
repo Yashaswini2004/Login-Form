a618f1ad34f863edcf7e82bb92c72e6f61e890a4
Yashaswini2004 <yashaswinic31@gmail.com>
2025-01-05 19:22:22 +0530

    complete code

---
+++ index.js
@@ -0,0 +1,14 @@
+const showPopupBtn=document.querySelector(".login-btn");
+const formPopup=document.querySelector(".form-popup");
+const hidePopupBtn=document.querySelector(".form-popup .close-btn");
+const loginSignupLink=document.querySelectorAll(".form-box .botton-link a");
+showPopupBtn.addEventListener("click",()=>{
+    document.body.classList.toggle("show-popup");
+});
+hidePopupBtn.addEventListener("click",()=> showPopupBtn.click());
+loginSignupLink.forEach(link=>{
+    link.addEventListener("click",(e)=>{
+        e.preventDefault();
+        formPopup.classList[link.id==="signup-link" ? 'add':'remove']("show-signup");
+    });
+});
